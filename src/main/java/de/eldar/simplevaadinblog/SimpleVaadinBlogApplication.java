4037663af6dee13290ed02ee0f7a509e9ed4530d
Torsten Kehrer <kehrer.torsten@gmail.com>
2018-07-26 21:22:43 +0200

    First iitializr project

---
+++ src/main/java/de/eldar/simplevaadinblog/SimpleVaadinBlogApplication.java
@@ -0,0 +1,12 @@
+package de.eldar.simplevaadinblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleVaadinBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleVaadinBlogApplication.class, args);
+	}
+}
---
+++ src/test/java/de/eldar/simplevaadinblog/SimpleVaadinBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package de.eldar.simplevaadinblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleVaadinBlogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
